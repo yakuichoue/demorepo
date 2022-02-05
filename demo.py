54b44f3a2ee7215095ee681784dc4d9c456f5dee
aakuichoue <yakuichoue@gmail.com>
2022-02-05 02:42:10 -0500

    first commit here

---
+++ demo.py
@@ -0,0 +1,13 @@
+import sys
+import site
+
+print(site.getsitepackages())   
+print(sys.version)
+print(sys.executable)
+print("Hello World")
+print ("Hello W00orld")
+x = 5
+if x == 6:
+    print("Hello World 2")
+else:
+    print("You lost rire")
